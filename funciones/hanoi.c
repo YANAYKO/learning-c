2a09a0ca2a9b94081cc9294e62e3c85eb89cfa07
Luis Vittorio <luis_abv@outlook.com>
2020-06-27 14:25:15 -0500

    comentario explicativo

--- funciones/hanoi.c
+++ funciones/hanoi.c
@@ -26,8 +26,16 @@ void hanoi(int nro_discos, int barra_inicial, int barra_central, int barra_final
     {
         printf("%d -> %d\n", barra_inicial, barra_final);
     }else {
+        /* 
+            es la primera llamada para trasladar todos los discos de la primera barra a la segunda
+            barra, usando como apoyo a la tercera barra
+        */
         hanoi(nro_discos - 1, barra_inicial, barra_final, barra_central);
         printf("%d -> %d\n", barra_inicial, barra_final);
+        /*
+            esta es la segunda llamada para trasladar los discos que se encuentran en la segunda barra
+            a la tercera barra, usando como apoyo a la primera barra
+        */
         hanoi(nro_discos - 1, barra_central, barra_inicial, barra_final);
     }
 }
