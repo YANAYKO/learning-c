2caa4c0eb458c0cf697b8c583f678e92edbe6b5f
Luis Vittorio <luis_abv@outlook.com>
2020-07-05 21:35:53 -0500

    ejercicio resuelto

--- funciones/_5_47.c
+++ funciones/_5_47.c
@@ -14,10 +14,138 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+enum Estado {CONTINUA, GANA, PIERDE};
+
+int lanzarDados(void);
+void jugar(unsigned int apuesta);
+
+int bank_Balance = 1000;
 
 int main(void)
 {
+    // aleatoriza el generador de números aleatorios usando la hora actual
+    srand(time(NULL));
+
     
+    int sum;
+    unsigned int apuesta;
+    char seguir_jugando;
+
+    puts("Craps game con apuesta.");
+
+    printf("Ingrese una apuesta (<= 1000): ");
+    scanf("%u", &apuesta);
+
+    while (apuesta > 1000)
+    {
+        if (apuesta > 1000)
+            printf("Ingrese una apuesta valida.\n");
+        
+        printf("Ingrese una apuesta (<= 1000): ");
+        scanf("%u", &apuesta);
+    }
+
+
+    jugar(apuesta);
+
+    do {
+        
+        if (bank_Balance == 0)
+        {
+            puts("Sorry. You busted!");
+            break;
+        }
+        printf("Desea seguir jugando? (s/n): ");
+        scanf(" %c", &seguir_jugando);
+
+        if (seguir_jugando == 's')
+        {
+            printf("Ingrese una apuesta (<= %d): ", bank_Balance);
+            scanf("%u", &apuesta);
+            jugar(apuesta);
+        }
+    } while (seguir_jugando != 'n');
+    
+
+    if (bank_Balance > 0)
+    {
+        printf("Obteniste:  %d puntos\n", bank_Balance);
+    }
+
+    //printf("%u\n", apuesta);
     return EXIT_SUCCESS;
+}
+
+void jugar(unsigned int apuesta)
+{
+    int miPuntaje;
+    enum Estado estadoJuego;
+    int sum = lanzarDados(); // primera jugada
+
+    // determina el estado del juego basado en la suma de los dados
+    switch (sum) 
+    {
+        // gana en el primer lanzamiento
+        case 7:
+        case 11:
+            estadoJuego = GANA;
+            bank_Balance += apuesta;
+            printf("Dinero: %u\n", bank_Balance);
+            break;
+        // pierde en el primer lanzamiento
+        case 2:
+        case 3:
+        case 12:
+            estadoJuego = PIERDE;
+            bank_Balance -= apuesta;
+            printf("Dinero: %u\n", bank_Balance);
+            break;
+        
+        // almacena el puntaje
+        default:
+            estadoJuego = CONTINUA;
+            miPuntaje = sum;
+            puts("Aw cmon, take a chance!");
+            printf("Puntaje es: %d\n", miPuntaje);
+            break;
+    }
+
+    while (CONTINUA == estadoJuego && bank_Balance > 0)
+    {
+        sum = lanzarDados();
+        //determina el estado del juego
+        if (sum == miPuntaje)
+        {
+            estadoJuego = GANA;
+            bank_Balance += apuesta;
+            printf("Dinero:: %u\n", bank_Balance);
+        } else {
+            if (7 == sum)
+            {
+                estadoJuego = PIERDE;
+                bank_Balance -= apuesta;
+                
+                printf("Dinero:: %u\n", bank_Balance);
+            }
+        }
+    }
+
+    if (GANA == estadoJuego)
+    {
+        puts("You're up big. Now's the time to cash in your chips!");
+    } else  if (PIERDE == estadoJuego && bank_Balance > 0){
+        puts("Oh, you're going for broke, huh?");
+    }
+}
+
+int lanzarDados(void)
+{
+    int dado1 = 1 + (rand() % 6);
+    int dado2 = 1 + (rand() % 6);
+
+    printf("Player rolled %d + %d = %d\n", dado1, dado2, dado1 + dado2);
+    
+    return dado1 + dado2;
 }
