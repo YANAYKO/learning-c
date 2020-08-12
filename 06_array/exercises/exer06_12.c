b60bf3237bc0cdfdb8f06881f05356dbdefc6ec7
Luis Armando Bailon Victorio <30305990+r4ndx@users.noreply.github.com>
2020-08-12 18:08:01 -0500

    commit

---
+++ 06_array/exercises/exer06_12.c
@@ -0,0 +1,16 @@
+/*
+    Write loops that perform each of the following one-dimensional array operations:
+        a) Read the 20 elements of double array  sales from the keyboard.
+        b) Add 1000 to each of the 75 elements of double array  allowance.
+        c) Initialize the 50 elements of integer array  numbers to zero.
+        d) Print the 10 values of integer array  GPA in column format.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+    return EXIT_SUCCESS;
+}
